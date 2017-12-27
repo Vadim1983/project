7653bcf1f9fe0565ffade83f1b9258853ee6a63c
Vadim1983 <Vshhukin@mail.ru>
2017-12-27 21:54:05 +0300

    add carousel

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function() {
+  $('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    speed: 1000,
+    dots: true
+  });
+});
